7c1244c5d214cc8c5e4c01c0059840088012b43e
mahesh <mahesh12@gmail.com>
2022-04-07 16:11:24 +0530

    my first commit

---
+++ student.java
@@ -0,0 +1,15 @@
+public class student {
+}
+class student
+{
+    int id;
+    string name;
+}
+class test student1
+{
+    public static void main (string args[]){
+        student s1=new student();
+        system.out.println(s1.id);
+        system.out.println(s1.name);
+    }
+}
