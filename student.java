dce61bdd3b5bcbd850c25f8c266dbfc13468190c
mahesh <mahesh12@gmail.com>
2022-04-07 16:20:48 +0530

    this my second commit

--- student.java
+++ student.java
@@ -11,5 +11,6 @@ class test student1
         student s1=new student();
         system.out.println(s1.id);
         system.out.println(s1.name);
+        System.out.println("hello world this is mahesh")
     }
 }
